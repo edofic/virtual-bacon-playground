22116413cf4361eee8ec0055a0afa50a08c09675
Andraz Bajt <andraz@bajt.me>
2015-04-17 19:42:51 +0200

    events with dom delegator

--- js/main.js
+++ js/main.js
@@ -3,25 +3,51 @@ var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
 var createElement = require('virtual-dom/create-element');
 var Bacon = require('baconjs');
+var Delegator = require('dom-delegator')
 window.Bacon = Bacon;
+window.Delegator = Delegator;
+
 
 // model
-var streamCount = Bacon
-  .repeatedly(0, [1])
-  .scan(0, function(count, step) { return (count + step) % 200 });
+var clicks = new Bacon.Bus();
+var streamCount = clicks
+  .scan(0, function(count, click) { return count + 1 });
+
+var textInputBus = new Bacon.Bus();
+var textInput = textInputBus.startWith("");
+
 
 // view
-var streamTree = streamCount.map(function (count) {
+var clicker = streamCount.map(function (count) {
     return h('div', {
         style: {
             textAlign: 'center',
             lineHeight: '100px', 
             border: '1px solid red',
-            width: (100 + count) + 'px',
+            width: '100px',
             height: '100px'
+        },
+        'ev-click': function () {
+          clicks.push(1);
         }
     }, [String(count)]);
 });
+
+var echoer = textInput.map(function(textInputValue){
+    var input = h('input', {
+      type: "text",
+      "ev-input": function(ev) {
+        textInputBus.push(ev.target.value);
+      }
+    });
+    var txt = " text:" + textInputValue;
+    return h('div', [input, txt])
+});
+
+var streamTree = Bacon.combineWith(function(clicker, echoer) {
+  return h('div', [clicker, echoer]);
+}, clicker, echoer);
+
 
 //wiring
 var latestTree = undefined;
@@ -45,3 +71,5 @@ var unsub = streamTree.forEach(function (tree) {
     root = patch(root, diff(latestTree, nextTree));
   });
 });
+
+var delegator = Delegator();
